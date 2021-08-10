140b38a930fd058e8d44eec768951fd9847a64d2
Felipe Rocha <felipenrocha19@gmail.com>
2021-08-09 23:02:53 -0300

    no pivot almost done

--- main.py
+++ main.py
@@ -2,61 +2,69 @@
 # Teoria e Aplicacao de Grafos Trabalho 1
 
 
-
-
 def main():
+
     golfinhos_txt = read_file('soc-dolphins.txt')
     lista_adjacencia_golfinhos = create_dolphin_list(golfinhos_txt)
-
-    print(lista_adjacencia_golfinhos['62'])
-
-
-
+    P_incial = get_all_vertex(lista_adjacencia_golfinhos)
+    bron_kerb_algorithm_no_pivot(
+        R=[], P=P_incial, X=[])
 
 
-
-# def bron_kerb_algorithm_no_pivot(R, P, X):
-# """"
-# based on: https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
-
-
-#     algorithm BronKerbosch1(R, P, X) is
-
-#     if P and X are both empty then
-#         report R as a maximal clique
-#     for each vertex v in P do
-#         BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
-#         P := P \ {v}
-#         X := X ⋃ {v}
+def bron_kerb_algorithm_no_pivot(R, P, X):
+    """"
+    based on: https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
 
 
-# """
-    
+    algorithm BronKerbosch1(R, P, X) is
+    if P and X are both empty then
+        report R as a maximal clique
+    for each vertex v in P do
+        BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+        P := P \ {v}
+        X := X ⋃ {v}
+        """
+    golfinhos_txt = read_file('soc-dolphins.txt')
+    lista = create_dolphin_list(golfinhos_txt)
+
+#  Retirado dos slides das aulas:
+#  R: vértices que seriam parte do clique
+#  P: vértices que têm ligação com todos os vértices de R (candidatos).
+#  Conjunto X: vértices já analisados e que não  levam a uma extensão do conjunto R.
+    if len(P) == 0 and len(X) == 0:
+        # yield R
+        print(R)
+
+    for v in P[:]:
+        R_next = R
+        R_next = R.append(v)
+        # R_next.append(v)
+        P_next=intersection(P, neighbor(v, lista))
+        X_next=intersection(X, neighbor(v, lista))
+
+        bron_kerb_algorithm_no_pivot(R = R_next, P = P_next, X = X_next)
+        P.remove(v)
+        X.append(v)
 
 
-
-
-
-
-
-
+def neighbor(v, lista):
+    """retorna os vertices adjacentes a v"""
+    print(lista[str(v)])
+    return lista[str(v)]
 
 
 def read_file(filename):
-    """ 
+    """
     Funcao p ler o arquivo txt que servira de base de dados do grafo
     string :: IO
     filename -> arquivo
     """
-    arquivo = open(filename, "r")
+    arquivo=open(filename, "r")
 
-    golfinhos = arquivo.read()
+    golfinhos=arquivo.read()
 
     arquivo.close()
     return golfinhos
-
-
-
 
 
 def create_dolphin_list(txt):
@@ -65,84 +73,92 @@ def create_dolphin_list(txt):
     string :: list
     txt -> lista_golfinhos
 
-    lista_golfinhos = { 
+    lista_golfinhos = {
         vertice_1 : [vertices_adjacentes],
         vertice_2 : [vertices_adjacentes],
         ...
-        vertice_n : [vertices_adjacentes]         
+        vertice_n : [vertices_adjacentes]
         }
 
         vertices_adjacentes = [v1, v2, v3],    vn Inteiro
     """
 
-
-    lista_golfinhos = {}
-
+    lista_golfinhos={}
 
     # divide o texto a partir da string do arquivo, so precisamos da 2a posicao que eh onde estao os numeros:
-    txtNumeros = txt.split(
+    txtNumeros=txt.split(
         "62 62 159")[1]
-    
-    #dividindo os espaços para resultar em uma rray com os valores separados por um espaco ex.: ["11 1"]
-    txtArray = txtNumeros.split("\n")
+
+    # dividindo os espaços para resultar em uma rray com os valores separados por um espaco ex.: ["11 1"]
+    txtArray=txtNumeros.split("\n")
 
     # variavel responsavel por manter o vertice atual da lista
-    vertice_atual = ""
+    vertice_atual=""
     # array para os vertices adjacentes de cada vertice até o 55
-    vertices_adjacentes = []
+    vertices_adjacentes=[]
     for texto in txtArray:
 
+        vertices=texto.split(" ")
 
-
-        vertices = texto.split(" ")
-
-        
         # usando a segunda coluna como chave para vertice vertice[0]: vertice adjacente vertice[1]: vertice atual
         if len(vertices) == 2:
             if vertice_atual != vertices[1]:
                 # se o vertice for novo ele eh inserido na lista e eh definido como vertice atual e os vertices adjacentes sao zerados
-                vertice_atual = vertices[1]
-                vertices_adjacentes = []
+                vertice_atual=vertices[1]
+                vertices_adjacentes=[]
                 vertices_adjacentes.append(int(vertices[0]))
-                lista_golfinhos[vertice_atual] = vertices_adjacentes
-         
+                lista_golfinhos[vertice_atual]=vertices_adjacentes
+
             else:
                 # se vertice ja existir na lista, adiciona o novo vertice adjacente
-                vertices_adjacentes = lista_golfinhos.get(vertice_atual)
+                vertices_adjacentes=lista_golfinhos.get(vertice_atual)
                 vertices_adjacentes.append(int(vertices[0]))
-                lista_golfinhos[vertice_atual] = vertices_adjacentes
+                lista_golfinhos[vertice_atual]=vertices_adjacentes
 
-
-
- #segundo for para os vertices que nao aparecem na segunda coluna:
+ # segundo for para os vertices que nao aparecem na segunda coluna:
     for texto in txtArray:
-    
 
+        vertices=texto.split(" ")
 
-        vertices = texto.split(" ")
-
-        
         # usando a segunda coluna como chave para vertice vertice[1]: vertice adjacente vertice[0]: vertice atual
         if len(vertices) == 2:
-            vertice_atual = vertices[0]
+            vertice_atual=vertices[0]
             if not lista_golfinhos.get(vertice_atual):
                 # se o vertice for novo ele eh inserido na lista e eh definido como vertice atual e os vertices adjacentes sao zerados
-         
-                vertices_adjacentes = []
+
+                vertices_adjacentes=[]
                 vertices_adjacentes.append(int(vertices[1]))
-                lista_golfinhos[vertice_atual] = vertices_adjacentes
-         
+                lista_golfinhos[vertice_atual]=vertices_adjacentes
+
             else:
                 # se vertice ja existir na lista, adiciona o novo vertice adjacente
-                vertices_adjacentes = lista_golfinhos.get(vertice_atual)
+                vertices_adjacentes=lista_golfinhos.get(vertice_atual)
                 if int(vertices[0]) not in vertices_adjacentes:
                     vertices_adjacentes.append(int(vertices[1]))
-                lista_golfinhos[vertice_atual] = vertices_adjacentes
-
-
-
+                lista_golfinhos[vertice_atual]=vertices_adjacentes
 
     return lista_golfinhos
+
+
+def get_all_vertex(lista):
+    """retorna P (all vertex from adjacency list)"""
+    vertices=[]
+    for key, value in lista.items():
+        vertices.append(key)
+    return vertices
+
+
+# Python program to illustrate the intersection
+# of two lists in most simple way
+def intersection(lst1, lst2):
+    # copied from https://www.geeksforgeeks.org/python-intersection-two-lists/ and
+    # https://www.geeksforgeeks.org/python-union-two-lists/
+    lst3=[value for value in lst1 if value in lst2]
+    return lst3
+
+
+def union(lst1, lst2):
+    return lst1 + lst2
 
 
 main()
